572c13b3fbd3cc25b3118f2248c7613508a30809
rainjia <595610708@qq.com>
2022-03-25 02:56:01 -0700

    lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,16 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) return 0;
+    node* tortoise = head;
+    node* hare = head;
+    while (1) {
+    	hare = hare -> next;
+    	if (hare == NULL) return 0;
+    	hare = hare -> next;
+    	if (hare == NULL) return 0;
+    	tortoise = tortoise -> next;
+    	if (tortoise == hare) return 1;
+    }
     return 0;
 }
